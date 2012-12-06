5c999e3014e080155bfa1469fec90230dffffa33
Justin <justin.elden@gmail.com>
2012-12-06 08:58:55 -0500

    transporting

--- Scripts/Server.cs
+++ Scripts/Server.cs
@@ -1,33 +1,57 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using System.Net;
 using System.Net.Sockets;
 
 public class Server : MonoBehaviour {
 	
+	static Server _this;
+	
+	void Awake () {
+		_this = this;	
+	}
+	
 	void Start () {
-		BeginConnect("guildbros.com", 1337);
+		BeginConnect("localhost", 1337);
+	}
+	
+	Socket thisSocket = null;
+	
+	public static void Send(Byte[] bytes) {
+		_this.thisSocket.BeginSend(bytes, 0, bytes.Length, 0, SendCallback, 0);
+	}
+	
+	public static void SendCallback(IAsyncResult ar) {
+		// Debug.Log ("sent");
+	}
+	
+	public static void ConnectCallback(IAsyncResult ar)
+	{		
+	    _this.thisSocket = (Socket) ar.AsyncState;
+	    _this.thisSocket.EndConnect(ar);
 	}
 	
 	public static void BeginConnect(string host, int port)
 	{
-		// set up a new bsd socket for a streaming TCP/IP connection
-		Socket sock = new 
-			Socket(				
-				AddressFamily.InterNetwork,
-		 		SocketType.Stream, 
-		 		ProtocolType.Tcp
-				);  	
+	    IPAddress[] IPs = Dns.GetHostAddresses(host);
+	
 		
-		// set up ascii encoding then use it to encode an HTTP GET request
-		System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
-    	byte[] bytes = encoding.GetBytes("GET / HTTP/1.1\n\n");
-		
-		// connect and then send (synchronously)
-		sock.Connect(host, port);
-		sock.Send(bytes);
-		
-		// console output
-		Debug.Log ("Done");
+	    Socket s = new Socket(AddressFamily.InterNetwork,
+	        SocketType.Stream,
+	        ProtocolType.Tcp);
+	    s.BeginConnect(IPs[0], port, 
+	        new AsyncCallback(ConnectCallback), s);
+
+	}
+	
+	void Update() {
+		if (_this.thisSocket != null) {
+			if (_this.thisSocket.Available > 0) {
+				Byte[] buf = new Byte[_this.thisSocket.Available];
+				_this.thisSocket.Receive(buf, buf.Length, 0);
+				Debug.Log("received: " + buf.ToString());
+			}
+		}
 	}
 }
